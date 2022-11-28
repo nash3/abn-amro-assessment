207d975fe99e767e8592dc925074a2dcf2efa8d0
Vimbainashe Nhira <nhiralee@gmail.com>
2022-11-28 19:21:09 +0100

    add custom deserializer for dates

--- src/main/java/com/nhira/abnrecipeapp/AbnRecipeAppApplication.java
+++ src/main/java/com/nhira/abnrecipeapp/AbnRecipeAppApplication.java
@@ -1,7 +1,20 @@
 package com.nhira.abnrecipeapp;
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 public class AbnRecipeAppApplication {
@@ -9,5 +22,31 @@ public class AbnRecipeAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AbnRecipeAppApplication.class, args);
     }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(OffsetDateTime.class, new JsonSerializer<>() {
+            @Override
+            public void serialize(OffsetDateTime offsetDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+                jsonGenerator.writeString(DateTimeFormatter.ISO_ZONED_DATE_TIME.format(offsetDateTime));
+            }
+        });
+        simpleModule.addDeserializer(OffsetDateTime.class, new JsonDeserializer<>() {
+            @Override
+            public OffsetDateTime deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException, JacksonException {
+                return OffsetDateTime.parse(parser.getText(), DateTimeFormatter.ISO_ZONED_DATE_TIME);
+            }
+        });
+        objectMapper.registerModule(simpleModule);
+
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+
 
 }
